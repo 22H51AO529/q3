ea1cb045080ce7897a228fa383d34c1f3d640e55
Rakshitha Kailasa <kailasarakshitha@gmail.com>
2024-11-27 19:00:59 +0530

    initial commit

---
+++ Prime.java
@@ -0,0 +1,26 @@
+public class Prime 
+{
+    public static void main(String[] args) {
+        int limit = 50; // Change this to any number to set the upper limit
+        System.out.println("Prime numbers up to " + limit + ":");
+        
+        for (int number = 2; number <= limit; number++) {
+            boolean isPrime = true;
+            
+            for (int divisor = 2; divisor <= Math.sqrt(number); divisor++) {
+                if (number % divisor == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            
+            if (isPrime) {
+                System.out.print(number + " ");
+            }
+        }
+        System.out.println("prime");
+            System.out.println("prime number");
+        System.out.println("hello");
+        System.out.println("job prime");
+    }
+}
